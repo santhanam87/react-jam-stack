e62e6485d53089f327755dfc51b6845fdec20363
Santhanam Elumalai <sansandy04@gmail.com>
2024-09-02 12:12:06 -0600

    cleanup

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,15 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,10 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang='en'>
+      <body>{children}</body>
     </html>
   );
 }
